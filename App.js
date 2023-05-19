b9e359b0262d9b770f3fe6617b5c2103e79603fa
DP2807 <121560898+DP2807@users.noreply.github.com>
2023-05-19 17:54:09 +0700

    Update App.js

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          contact ig: @datt
+          contact ig: 
         </a>
       </header>
     </div>
