792af8451f586435785eef25fd1f74eb58fbab5a
DP2807 <121560898+DP2807@users.noreply.github.com>
2023-05-19 17:51:04 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Pham Tan Dat
+          -2993
+        
+        </p>
+        <a
+          className="App-link"
+          href="https://www.instagram.com/datt.2807/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          contact ig: @datt2807
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
